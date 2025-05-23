71ba76e5e004b50443dd510ca8ce116346177cd6
cjmaret <cjmaret@gmail.com>
2025-05-22 18:10:59 -0600

    decrease whisper model to small

--- paths.py
+++ paths.py
@@ -3,7 +3,7 @@ import os
 # define paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-MODEL_SIZE = "medium"  # options: "tiny", "base", "small", "medium", "large"
+MODEL_SIZE = "small"  # options: "tiny", "base", "small", "medium", "large"
 
 # ensure data directory exists
 # for local file storage
